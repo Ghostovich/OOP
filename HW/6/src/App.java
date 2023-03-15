cd506fb03913965876db74037fa3694998d3ecb0
Ghostovich <gusarov_konstant@mail.ru>
2023-03-15 14:13:24 +0300

    h6.1

--- HW/6/src/App.java
+++ HW/6/src/App.java
@@ -4,20 +4,7 @@
 public class App {
 
   public static void main(String[] args) {
-
-
     Menu.menu("Выберите действие: \n 1 - сложение\n 2 - вычитание\n 3 - умножение\n 4 - деление");
-
-
-    // Random random = new Random();
-    // Calc c = new Calc(new Logger());
-
-    // System.out.println(c.sub(new Operand(random.nextInt(100)), new Operand(random.nextInt(100))));
-    // System.out.println(c.div(new Operand(random.nextInt(100)), new Operand(random.nextInt(100))));
-    // System.out.println(c.div(new Operand(0), new Operand(0)));
-    // System.out.println(c.sub(new Operand(random.nextInt(100)), new Operand(random.nextInt(100))));
-    // System.out.println(c.mult(new Operand(random.nextInt(100)), new Operand(random.nextInt(100))));
-
   }
 }
 
