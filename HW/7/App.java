331f1c9bd31f81dec720db53a33790a68d2e6bc3
Ghostovich <gusarov_konstant@mail.ru>
2023-03-19 19:44:22 +0300

    h7

---
+++ HW/7/App.java
@@ -0,0 +1,25 @@
+// Заставить работать. (помним про SOLID)
+
+public class App {
+
+  public static void main(String[] args) {
+    new Innerapp(null).write();
+  }
+}
+
+interface IInterface {
+  void print();
+}
+
+class Innerapp {
+  IInterface v;
+
+  public Innerapp(IInterface v) {
+    this.v = v;
+  }
+
+  void write() {
+    v.print();
+  }
+
+}
